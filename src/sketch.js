0b8c88dee15b04ec98da8d1020fd24d1ed452c7c
Cayman Jeffers <cayman.jeffers@concept11studios.com>
2021-04-23 15:33:45 -0400

    final changes to two tone audio functionality before test build

--- src/sketch.js
+++ src/sketch.js
@@ -20,6 +20,9 @@ let mic;
 let pitch;
 let shouldRecord = true;
 let departments = []
+
+let triggeredDepartment
+
 getDepartments()
 
 async function getDepartments() {
@@ -79,6 +82,7 @@ function stoprecording(){
     document.querySelector('#audioFile').src = src
 
     sendAudioDispatch(blob)
+    triggeredDepartment = null
   };
   recorder.stop();
   shouldRecord = true
@@ -101,32 +105,37 @@ function getPitch() {
     // should record ensures that the startRecording function is only triggered once.
     if (frequency) {
       // step 1, get the triggered department from the first tone that matches the current frequency
-      let triggeredDepartment = _.find(departments, (department) => {
-        let toneOne = department.tones[0]
-        if (toneOne) {
-          let varianceHigh = toneOne.value + toneOne.variance
-          let varianceLow = toneOne.value - toneOne.variance
-          return (frequency <= varianceHigh) && (frequency >= varianceLow)
-        }
-      })
+      if (!triggeredDepartment) {
+        // set the triggered department as a global variable so it stays as triggered while the listending for the second tone
+        triggeredDepartment = _.find(departments, (department) => {
+          let toneOne = department.tones[0]
+          if (toneOne && !triggeredDepartment) {
+            let variance1High = parseFloat(toneOne.value) + parseFloat(toneOne.variance)
+            let variance1Low = parseFloat(toneOne.value) - parseFloat(toneOne.variance)
+            return (frequency <= variance1High) && (frequency >= variance1Low)
+          }
+        })
+      }
+
       // step 2 send the dispatch if one tone is set, otherwise test the other conditions.
       if (triggeredDepartment) {
         select('#result').html(triggeredDepartment.name);
         let toneTwo = triggeredDepartment.tones[1]
         if (toneTwo) {
-          let varianceHigh = toneTwo.value + toneTwo.variance
-          let varianceLow = toneTwo.value - toneTwo.variance
-          if ((frequency <= varianceHigh) && (frequency >= varianceLow)) {
+          let variance2High = parseFloat(toneTwo.value) + parseFloat(toneTwo.variance)
+          let variance2Low = parseFloat(toneTwo.value) - parseFloat(toneTwo.variance)
+          console.log(variance2High, variance2Low, frequency)
+          if ((frequency <= variance2High) && (frequency >= variance2Low)) {
             startrecording()
           }
         } else {
           startrecording()
         }
       } else {
-        select('#result').html(frequency);
+        select('#result').html(frequency.toFixed(2));
       }
     } else {
-      select('#result').html('');
+      select('#result').html('No Tone');
     }
     getPitch();
   })
